05e197e32ac9c27fd6f3b4e84f36011bd3a1e2c1
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-05-09 13:00:52 +0100

    refactor: create-assignments: extract functions: create_assignments/save_assignments

--- create-assignments.py
+++ create-assignments.py
@@ -23,10 +23,6 @@ TOP_N = 10
 # How many of each category we should collect
 K = 9
 
-# How many scenarios will be evaluated in total:
-TOTAL = TOP_N * K
-assert K % 3 == 0, "Number of scenarios per category must be divisible by 3 raters"
-
 # Top PEM categories, in order from most frequent to least frequent
 TOP_ERRORS = [
     ("compiler.err.premature.eof"),
@@ -54,40 +50,61 @@ TOP_ERRORS = [
 with open("sample.pickle", "rb") as f:
     ALL_SCENARIOS = pickle.load(f)
 
-# Group all scenarios by PEM category
-scenarios = defaultdict(list)
-for scenario in ALL_SCENARIOS:
-    scenarios[scenario["pem_category"]].append(scenario)
 
-# Time to assign scenarios to raters!
-rater1 = []
-rater2 = []
-rater3 = []
-for category in TOP_ERRORS[:TOP_N]:
-    # Retain only the top K scenarios from each category
-    for i, scenario in enumerate(scenarios[category][:K]):
-        # Rater 1 gets first 2/3
-        # Rater 2 gets last 2/3
-        # Rater 3 gets first 1/3 and last 1/3
-        third = i % 3
-        if third == 0:
-            rater1.append(scenario)
-            rater3.append(scenario)
-        elif third == 1:
-            rater1.append(scenario)
-            rater2.append(scenario)
-        else:
-            rater2.append(scenario)
-            rater3.append(scenario)
+def create_assignments(top_n: int, k: int):
+    """
+    Create assignments for the top N error message categories. k MUST be a multiple of
+    3 such that we can divide the assignments in thirds.
+    """
 
-ONE_THIRD = TOTAL // 3
-assert len(rater1) == len(rater2) == len(rater3) == ONE_THIRD * 2
+    # How many scenarios will be evaluated in total:
+    total = top_n * k
+    assert k % 3 == 0, "Number of scenarios per category must be divisible by 3 raters"
 
-# Save them out:
-for assignments, name in zip([rater1, rater2, rater3], ["eddie", "prajish", "brett"]):
-    with open(f"{name}-assignments.tsv", "w") as f:
-        for scenario in assignments:
-            pem_category = scenario["pem_category"]
-            srcml_path = scenario["xml_filename"]
-            version = scenario["version"]
-            f.write(f"{pem_category}\t{srcml_path}\t{version}\n")
+    # Group all scenarios by PEM category
+    scenarios = defaultdict(list)
+    for scenario in ALL_SCENARIOS:
+        scenarios[scenario["pem_category"]].append(scenario)
+
+    # Time to assign scenarios to raters!
+    rater1 = []
+    rater2 = []
+    rater3 = []
+    for category in TOP_ERRORS[:TOP_N]:
+        # Retain only the top K scenarios from each category
+        for i, scenario in enumerate(scenarios[category][:K]):
+            # Rater 1 gets first 2/3
+            # Rater 2 gets last 2/3
+            # Rater 3 gets first 1/3 and last 1/3
+            third = i % 3
+            if third == 0:
+                rater1.append(scenario)
+                rater3.append(scenario)
+            elif third == 1:
+                rater1.append(scenario)
+                rater2.append(scenario)
+            else:
+                rater2.append(scenario)
+                rater3.append(scenario)
+
+    one_third = total // 3
+    assert len(rater1) == len(rater2) == len(rater3) == one_third * 2
+
+    return rater1, rater2, rater3
+
+
+def save_assignments(rater1, rater2, rater3):
+    for assignments, name in zip(
+        [rater1, rater2, rater3], ["eddie", "prajish", "brett"]
+    ):
+        with open(f"{name}-assignments.tsv", "w") as f:
+            for scenario in assignments:
+                pem_category = scenario["pem_category"]
+                srcml_path = scenario["xml_filename"]
+                version = scenario["version"]
+                f.write(f"{pem_category}\t{srcml_path}\t{version}\n")
+
+
+if __name__ == "__main__":
+    rater1, rater2, rater3 = create_assignments(TOP_N, K)
+    save_assignments(rater1, rater2, rater3)
